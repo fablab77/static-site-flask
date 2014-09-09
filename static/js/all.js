c120810cf7c0ef8be190893abb5165fd2c3b79b4
Ksiusha <ksiusha5@gmail.com>
2014-09-09 13:04:57 +0300

    first

---
+++ static/js/all.js
@@ -0,0 +1,8 @@
+$('document').ready(function(){
+	// $('.pinned').pin({containerSelector: '.container'});
+
+	// $('.header').on('hover', function(){
+		// console.log('hover');
+		// $('.fotorama').hover();
+	// });
+});
